d0720d44236f26bbefa1ec5f96baf404545e3790
xthuang_293 <xthuang5556019@163.com>
2015-12-28 15:05:03 +0800

    解决按钮引用路径不正确问题;

--- HBT_UIShapedButton/HBT_UIShapedButton.h
+++ HBT_UIShapedButton/HBT_UIShapedButton.h
@@ -8,6 +8,11 @@
 
 #import <UIKit/UIKit.h>
 
+typedef enum : NSUInteger {
+    HBT_UIShapedButtonTypeOutArrow = 0,
+    HBT_UIShapedButtonTypeInArrow,
+} UIShapedButtonType;
+
 /**
 HBT_UIShapedButton 使用贝塞尔路径绘制不规则的按钮,
 并在绘制范围内相应事件
@@ -15,25 +20,9 @@ HBT_UIShapedButton 使用贝塞尔路径绘制不规则的按钮,
 @interface HBT_UIShapedButton : UIButton
 
 /**
- *  定义路径, 可以通过现有方法创建, 外部引用可以利用
- *  path 创建需要的形状
+ *  按钮类型  默认HBT_UIShapedButtonTypeOutArrow  
  */
-@property(nonatomic, strong) UIBezierPath *path;
+@property (nonatomic) UIShapedButtonType shapeType;
 
-/**
- *  创建右边突出箭头的按钮
- *
- *  @param width   按钮宽
- *  @param height  按钮高
- */
-- (void)hbt_createRightOutArrowOfButtonWithWidth:(CGFloat)width height:(CGFloat)height;
-
-/**
- *  创建左边凹陷箭头的按钮
- *
- *  @param width   按钮宽
- *  @param height  按钮高
- */
-- (void)hbt_createLeftInArrowOfButtonWithWidth:(CGFloat)width height:(CGFloat)height;
 
 @end
--- HBT_UIShapedButton/HBT_UIShapedButton.m
+++ HBT_UIShapedButton/HBT_UIShapedButton.m
@@ -8,8 +8,30 @@
 
 #import "HBT_UIShapedButton.h"
 
-@implementation HBT_UIShapedButton
+@interface HBT_UIShapedButton ()
 
+/**
+ *  定义路径, 可以通过现有方法创建, 外部引用可以利用
+ *  path 创建需要的形状
+ */
+@property(nonatomic, strong) UIBezierPath *shapePath;
+
+@end
+
+@implementation HBT_UIShapedButton
+-(instancetype)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        _shapeType = HBT_UIShapedButtonTypeOutArrow;
+    }
+    return self;
+}
+-(void)setShapeType:(UIShapedButtonType)shapeType
+{
+    _shapeType = shapeType;
+    [self setNeedsLayout];
+}
 /**
  *  绘制按钮时添加 path 遮罩
  *
@@ -18,8 +40,11 @@
 - (void)drawRect:(CGRect)rect
 {
     [super drawRect:rect];
+    //update shapePath with rect
+    self.shapePath = [self makePathWithType:self.shapeType inRect:rect];
+    //
     CAShapeLayer *layer = [CAShapeLayer layer];
-    layer.path = self.path.CGPath;
+    layer.path = self.shapePath.CGPath;
     self.layer.mask = layer;
 }
 
@@ -33,40 +58,124 @@
  */
 - (BOOL)pointInside:(CGPoint)point withEvent:(UIEvent *)event
 {
-    BOOL res = [super pointInside:point withEvent:event];
-    if (res) {
-        if ([self.path containsPoint:point]) {
-            return YES;
-        }
-        return NO;
+    if (!self.shapePath) {
+        return [super pointInside:point withEvent:event];
+    }
+    //
+    if ([super pointInside:point withEvent:event] && [self.shapePath containsPoint:point]) {
+        return YES;
     }
     return NO;
 }
-
-- (void)hbt_createRightOutArrowOfButtonWithWidth:(CGFloat)width height:(CGFloat)height
+#define TITLE_LABEL_TAG 1234
+- (CGRect)titleRectForContentRect:(CGRect)contentRect
 {
-    UIBezierPath *path = [UIBezierPath bezierPath];
-    [path moveToPoint:CGPointMake(0, 0)];
-    CGFloat keyWidth = (sqrt(3) * height) / 2 - 10;
-    [path addLineToPoint:CGPointMake(width - keyWidth, 0)];
-    [path addLineToPoint:CGPointMake(width, height / 2)];
-    [path addLineToPoint:CGPointMake(width - keyWidth, height)];
-    [path addLineToPoint:CGPointMake(0, height)];
-    [path closePath];
-    self.path = path;
+    // define the desired title inset margins based on the whole rect and its padding
+    UIEdgeInsets padding = [self titleEdgeInsets];
+    CGFloat offset = 0.f;
+    if (self.shapeType == HBT_UIShapedButtonTypeInArrow) {
+        offset = 10.f;
+    }
+    CGRect titleRect = CGRectMake(contentRect.origin.x    + padding.left + offset,
+                                  contentRect.origin.x    + padding.top,
+                                  contentRect.size.width  - (padding.right + padding.left),
+                                  contentRect.size.height - (padding.bottom + padding.top));
+    
+    // save the current title view appearance
+    NSString *title = [self currentTitle];
+    UIColor  *titleColor = [self currentTitleColor];
+    UIColor  *titleShadowColor = [self currentTitleShadowColor];
+//    UIFont *titleFont = self.titleLabel.font;
+    // we only want to add our custom label once; only 1st pass shall return nil
+    UILabel  *titleLabel = (UILabel*)[self viewWithTag:TITLE_LABEL_TAG];
+    
+    
+    if (!titleLabel)
+    {
+        // no custom label found (1st pass), we will be creating & adding it as subview
+        titleLabel = [[UILabel alloc] initWithFrame:titleRect];
+        [titleLabel setTag:TITLE_LABEL_TAG];
+        
+        // make it multi-line
+        [titleLabel setNumberOfLines:0];
+        [titleLabel setLineBreakMode:NSLineBreakByWordWrapping];
+        
+        // title appearance setup; be at will to modify
+        [titleLabel setBackgroundColor:[UIColor clearColor]];
+//        titleLabel.font = titleFont;
+        //[titleLabel setFont:[self font]];
+        [titleLabel setShadowOffset:CGSizeMake(0, 1)];
+        [titleLabel setTextAlignment:NSTextAlignmentCenter];
+        
+        [self addSubview:titleLabel];
+    }
+    
+    // finally, put our label in original title view's state
+    [titleLabel setText:title];
+    [titleLabel setTextColor:titleColor];
+    [titleLabel setShadowColor:titleShadowColor];
+    
+    // and return empty rect so that the original title view is hidden
+    return CGRectZero;
+//    CGRect frame = [super titleRectForContentRect:contentRect];
+//    if (self.shapeType == HBT_UIShapedButtonTypeInArrow) {
+//        frame.origin.x = CGRectGetMinX(frame) + 10.f;
+//    }
+//
+//    return frame;
+}
+#pragma mark - private method
+-(UIBezierPath *)makePathWithType:(UIShapedButtonType)type inRect:(CGRect)rect
+{
+    UIBezierPath *path = nil;
+    CGFloat buttonWidth = rect.size.width;
+    CGFloat buttonHeight = rect.size.height;
+    if (type == HBT_UIShapedButtonTypeOutArrow) {
+        path = [UIBezierPath bezierPath];
+        [path moveToPoint:CGPointMake(0, 0)];
+        CGFloat keyWidth = (sqrt(3) * buttonHeight) / 2.f - 10;
+        [path addLineToPoint:CGPointMake(buttonWidth - keyWidth, 0)];
+        [path addLineToPoint:CGPointMake(buttonWidth, buttonHeight / 2.f)];
+        [path addLineToPoint:CGPointMake(buttonWidth - keyWidth, buttonHeight)];
+        [path addLineToPoint:CGPointMake(0, buttonHeight)];
+        [path closePath];
+    } else {
+        path = [UIBezierPath bezierPath];
+        [path moveToPoint:CGPointMake(0, 0)];
+        CGFloat keyWidth = (sqrt(3) * buttonHeight) / 2 - 10;
+        [path addLineToPoint:CGPointMake(buttonWidth, 0)];
+        [path addLineToPoint:CGPointMake(buttonWidth, buttonHeight)];
+        [path addLineToPoint:CGPointMake(0, buttonHeight)];
+        [path addLineToPoint:CGPointMake(keyWidth, buttonHeight / 2)];
+        [path closePath];
+    }
+    return path;
 }
 
-- (void)hbt_createLeftInArrowOfButtonWithWidth:(CGFloat)width height:(CGFloat)height
-{
-    UIBezierPath *path = [UIBezierPath bezierPath];
-    [path moveToPoint:CGPointMake(0, 0)];
-    CGFloat keyWidth = (sqrt(3) * height) / 2 - 10;
-    [path addLineToPoint:CGPointMake(width, 0)];
-    [path addLineToPoint:CGPointMake(width, height)];
-    [path addLineToPoint:CGPointMake(0, height)];
-    [path addLineToPoint:CGPointMake(keyWidth, height / 2)];
-    [path closePath];
-    self.path = path;
-}
+//- (void)hbt_createRightOutArrowOfButtonWithWidth:(CGFloat)width height:(CGFloat)height
+//{
+//    UIBezierPath *path = [UIBezierPath bezierPath];
+//    [path moveToPoint:CGPointMake(0, 0)];
+//    CGFloat keyWidth = (sqrt(3) * height) / 2 - 10;
+//    [path addLineToPoint:CGPointMake(width - keyWidth, 0)];
+//    [path addLineToPoint:CGPointMake(width, height / 2)];
+//    [path addLineToPoint:CGPointMake(width - keyWidth, height)];
+//    [path addLineToPoint:CGPointMake(0, height)];
+//    [path closePath];
+//    self.path = path;
+//}
+//
+//- (void)hbt_createLeftInArrowOfButtonWithWidth:(CGFloat)width height:(CGFloat)height
+//{
+//    UIBezierPath *path = [UIBezierPath bezierPath];
+//    [path moveToPoint:CGPointMake(0, 0)];
+//    CGFloat keyWidth = (sqrt(3) * height) / 2 - 10;
+//    [path addLineToPoint:CGPointMake(width, 0)];
+//    [path addLineToPoint:CGPointMake(width, height)];
+//    [path addLineToPoint:CGPointMake(0, height)];
+//    [path addLineToPoint:CGPointMake(keyWidth, height / 2)];
+//    [path closePath];
+//    self.path = path;
+//}
 
 @end
